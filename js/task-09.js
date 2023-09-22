beb6cb907c31e15a6e338d1a2963d077336d12f3
turasova <134943646+turasova@users.noreply.github.com>
2023-09-22 15:33:28 +0300

    task-09

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,18 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+//Напиши скрипт, який змінює кольори фону елемента < body > через інлайн - стиль 
+//по кліку на button.change - color і виводить значення кольору в span.color.
+
+//Для генерування випадкового кольору використовуй функцію getRandomHexColor.
+
+const backgroundChangeBtn = document.querySelector('.change-color');
+
+backgroundChangeBtn.addEventListener('click', handlerBtnChange);
+
+function handlerBtnChange(event) {
+  const color = getRandomHexColor();
+  document.body.style.background = color;
+  document.querySelector('.color').textContent = color;
+  
+}
